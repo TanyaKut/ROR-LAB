b7ac123e4bf8790ed7a4873b753650e9b775ce9e
TanyaKut <tanikvetil.k@gmail.com>
2017-10-15 14:43:13 +0300

    sign_in sign_up

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+	validates :firstname, :lastname, :email, :password, presence: true
+	validates :email, uniqueness: true
+	validates :email, uniqueness: {case_sensitive: false}
+	validates :password, length: {in: 6..20}
+end
